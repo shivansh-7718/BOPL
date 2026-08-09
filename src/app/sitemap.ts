c2b500a9a5bc526830b83ebb48a59ded1c00fe20
Shivansh Tiwari <155485601+shivansh-7718@users.noreply.github.com>
2026-08-10 01:14:45 +0530

    Updated sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://biocyteorganics.com";
+
+  const routes = ["", "/about", "/products", "/careers", "/contact"];
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: route === "" ? "weekly" : "monthly",
+    priority: route === "" ? 1 : 0.8,
+  }));
+}
